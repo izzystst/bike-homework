48819dc605bf087de8372892c85c7fddcd5675eb
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-04-25 18:47:18 -0500

    added git ignore whooops

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-app.py
+from flask import Flask, jsonify
+
+import models
+
+DEBUG=True
+PORT=8000
+app =Flask(__name__) #ask if  this has to be name ?
+
+@app.route('/')
+def hi():
+	return 'hiiii'
+
+
+if __name__ == '__main__':
+	models.initialize()
+	app.run(debug=DEBUG, port=PORT)
