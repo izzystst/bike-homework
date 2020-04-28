aebf4416ae9c7075c46fa7e47728098a26aabefd
Izzy <IZZZZZY@Isobellas-MacBook-Pro-3.local>
2020-04-27 19:12:16 -0500

    CORS now added

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, jsonify
 from resources.bikes import bikes
 
 import models
-
+from flask_cors import CORS
 DEBUG=True
 PORT=8000
 app =Flask(__name__) #ask if  this has to be name ?
-
+CORS(bikes, origins=['http://localhost:3001'], supports_credentials=True)
 app.register_blueprint(bikes, url_prefix='/api/v1/bikes/')
 
 @app.route('/')
